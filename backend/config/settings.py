0af8b116130a9cf1d42d5dbb119804c26683670e
Vishal Gupta <mr.vishal4042@gmail.com>
2026-09-06 21:20:11 +0530

    Set up NeuroSync backend API

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
